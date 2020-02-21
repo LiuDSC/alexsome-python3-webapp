0a8b20d5e1bd595a17a19dd263af51b391ec83e2
Alex LIU <liuyuheng2015@126.com>
2020-02-21 20:19:47 +0800

    Day 5 Update

--- www/orm.py
+++ www/orm.py
@@ -7,14 +7,6 @@ import asyncio, logging
 
 import aiomysql
 
-'''
-创建连接池
-
-我们需要创建一个全局的连接池，每个HTTP请求都可以从连接池中直接获取数据库连接。
-使用连接池的好处是不必频繁地打开和关闭数据库连接，而是能复用就尽量复用。
-连接池由全局变量__pool存储，缺省情况下将编码设置为utf8，自动提交事务
-
-'''
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
 
@@ -34,12 +26,6 @@ async def create_pool(loop, **kw):
         loop = loop
 	)
 
-'''
-Select
-
-要执行SELECT语句，我们用select函数执行，需要传入SQL语句和SQL参数：
-'''
-
 async def select(sql, args, size=None):
     log(sql, args)
     global __pool
@@ -52,13 +38,6 @@ async def select(sql, args, size=None):
                 rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
-
-'''
-Insert, Update, Delete
-
-要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数，
-因为这3种SQL的执行都需要相同的参数，以及返回一个整数表示影响的行数：
-'''
 
 async def execute(sql, args, autocommit=True):
     log(sql)
@@ -76,14 +55,6 @@ async def execute(sql, args, autocommit=True):
                 await conn.rollback()
             raise
         return affected
-
-'''
-ORM
-
-有了基本的select()和execute()函数，我们就可以开始编写一个简单的ORM了。
-设计ORM需要从上层调用者角度来设计。
-我们先考虑如何定义一个User对象，然后把数据库表users和它关联起来。
-'''
 
 def create_args_string(num):
     L = []
