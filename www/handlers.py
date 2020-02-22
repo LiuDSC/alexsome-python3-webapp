ccb7c59435af1a5c185f2e88158a014df46c09da
Alex LIU <liuyuheng2015@126.com>
2020-02-23 01:24:19 +0800

    删除了一处注释

--- www/handlers.py
+++ www/handlers.py
@@ -350,30 +350,3 @@ async def api_delete_users(id, request):
             await c.update()
     id = id_buff
     return dict(id=id)
-
-'''
-@get('/')
-def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
-    ]
-    return {
-        '__template__': 'blogs.html',
-        'blogs': blogs
-    }
-
-@get('/api/users')
-async def api_get_users(*, page='1'):
-
-    num = await User.findNumber('count(id)')
-
-    if num == 0:
-        return dict(users=())
-    users = await User.findAll(orderBy='created_at desc')
-    for u in users:
-        u.passwd = '******'
-    return dict(users=users)
-'''
